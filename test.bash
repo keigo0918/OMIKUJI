0ac27949a601840f8fdbf26a76d35e48672caad2
KIG <147677592+keigo0918@users.noreply.github.com>
2024-01-04 21:47:20 +0900

    Create test.bash

---
+++ test.bash
@@ -0,0 +1,12 @@
+#!/bin/bash -xv 
+#SPDX-FileCopyrightText: 2024 Keigo Shishido
+#SPDX-License-Identifier: BSD-3-Clause
+
+out=$python3 omikuji.py
+おみくじを引きます・・・
+ガサッ
+ゴソッ
+ガサッ
+＿人人人人人人人人人＿
+＞       大吉       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣
