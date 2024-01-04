50210bee05458e98f8b5f6cecea40f51b7ed689f
KIG <147677592+keigo0918@users.noreply.github.com>
2024-01-04 21:51:04 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -3,10 +3,10 @@
 #SPDX-License-Identifier: BSD-3-Clause
 
 out=$python3 omikuji.py
-おみくじを引きます・・・
-ガサッ
-ゴソッ
-ガサッ
+おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
 ＿人人人人人人人人人＿
 ＞       大吉       ＜
 ￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣
