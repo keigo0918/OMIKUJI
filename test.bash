04b9ceff1f87ccc0b888321ce2ed1fe00a530338
KIG <147677592+keigo0918@users.noreply.github.com>
2024-01-06 15:56:23 +0900

    Update test.bash

--- test.bash
+++ test.bash
@@ -2,6 +2,12 @@
 #SPDX-FileCopyrightText: 2024 Keigo Shishido
 #SPDX-License-Identifier: BSD-3-Clause
 
+ng () {
+      echo NG at Line $program
+      res=1
+}
+res=0 
+
 out=$python3 program.py
 [ "${out}" = "おみくじを引きます
 ガラ･･･
@@ -10,3 +16,52 @@ out=$python3 program.py
 ＿人人人人人人人人人＿
 ＞       大吉       ＜
 ￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+out=$python3 program.py
+[ "${out}" = "おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
+＿人人人人人人人人人＿
+＞       吉       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+out=$python3 program.py
+[ "${out}" = "おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
+＿人人人人人人人人人＿
+＞       中吉       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+out=$python3 program.py
+[ "${out}" = "おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
+＿人人人人人人人人人＿
+＞       小吉       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+out=$python3 program.py
+[ "${out}" = "おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
+＿人人人人人人人人人＿
+＞       末吉       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+out=$python3 program.py
+[ "${out}" = "おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
+＿人人人人人人人人人＿
+＞       凶       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+out=$python3 program.py
+[ "${out}" = "おみくじを引きます
+ガラ･･･
+ガラ･･･
+ガラ･･･
+＿人人人人人人人人人＿
+＞       大凶       ＜
+￣Y^Y^Y^Y^Y^Y^Y^Y^Y￣" ] || ng{LINENO}
+exit $res
